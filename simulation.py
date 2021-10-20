844e17dcd6174e355301110e047535348a20c323
574445631@qq.com <574445631@qq.com>
2021-10-20 22:45:30 +0800

    临时提交

--- simulation.py
+++ simulation.py
@@ -1,3 +1,5 @@
+import json
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 # https://www.jianshu.com/p/1531e12f8852
@@ -6,14 +8,33 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome()
 
 # 通过浏览器访问链接
-browser.get('https://login.hand-china.com/sso/login?service=http%3A%2F%2Feip.hand-china.com%2Fc%2Fportal%2Flogin%3Fredirect%3D%252Fen%252Fpersonal%26refererPlid%3D50108%26p_l_id%3D20146')
+browser.get('http://eip.hand-china.com/web/guest/personal')
 
 # 找到对应的ID页面元素
 # 用户名
 browser.find_element_by_id("username").clear()
-browser.find_element_by_id("username").send_keys("****")
+browser.find_element_by_id("username").send_keys("23853")
 # 密码
 browser.find_element_by_id("password").clear()
-browser.find_element_by_id("password").send_keys("****")
+browser.find_element_by_id("password").send_keys("HWC&yzr1314")
 # 登录按钮
 browser.find_element_by_name("submit").send_keys(Keys.ENTER)
+
+# 等待3s 避免没有获取到日志信息系导致报错
+time.sleep(3)
+
+browser.get("http://app.hand-china.com/hrms/main.screen")
+# 获取请求的日志信息
+request_log = browser.get_log('performance')
+print("++++++++++++++++"+request_log)
+
+
+for i in range(len(request_log)):
+    message = json.loads(request_log[i]['message'])
+    message = message['message']['params']
+    # .get() 方式获取是了避免字段不存在时报错
+    request = message.get('request')
+    if(request is None):
+        continue
+    url = request.get('url')
+    print("----------------------------------"+url)
